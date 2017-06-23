6326c2513bbaebc06043994640fecdc5bb361627
Anuj Sachan <anujsachan@gmail.com>
2017-06-23 17:19:20 +0530

    Angular Material UI

--- app.js
+++ app.js
@@ -8,5 +8,29 @@ angular
       return function() {
         $mdSidenav(componentId).toggle();
       };
-    }
+    }  var ctrl = this;
+  ctrl.search = null;
+  ctrl.showPreSearchBar = function() {
+    return ctrl.search == null;
+  };
+  ctrl.initiateSearch = function() {
+    ctrl.search = '';
+  };
+  ctrl.showSearchBar = function() {
+    return ctrl.search != null
+  };
+  ctrl.endSearch = function() {
+    return ctrl.search = null;
+  };
+  ctrl.submit = function() {
+    console.error('Search function not yet implemented');
+  }
+
+  // to focus on input element after it appears
+  $scope.$watch(function() {
+    return document.querySelector('#search-bar:not(.ng-hide)');
+  }, function(){
+      document.getElementById('search-input').focus();
   });
+  });
+  
